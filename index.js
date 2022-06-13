a8630cf6a63f5e9a61cd9a39c3f7b4952e41970d
dishantzaveri <zaveridishant@gmail.com>
2022-06-13 19:44:15 +0530

    edit profileand post profile screen integrated

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
-import OtherInterests from './src/screens/OtherInterests';
 import {name as appName} from './app.json';
-import Signup from './src/screens/signup';
-import Login from './src/screens/Login';
-import Profile from './src/screens/Profile';
-import Groups from './src/screens/Groups';
-
 AppRegistry.registerComponent(appName, () =>App);
 
