0cc96db609c976750a23bbe59913e9faca0bea79
Riya Shah <riyajshah021@gmail.com>
2022-06-13 15:19:43 +0530

    events screen integrated

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -1,14 +1,15 @@
-import React ,{useState} from 'react';
+import React ,{useState,useEffect} from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    Image,
+    ImageBackground,
     Button,
     Alert,
     TextInput,
     Pressable,
     TouchableOpacity,
+    FlatList
 } from 'react-native';
 import Textfield from '../components/Textfield';
 import SelectDropdown from 'react-native-select-dropdown';
@@ -18,19 +19,56 @@ import {
     heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import { ScrollView } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Events({ navigation }) {
+    const [data, setdata]= useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-   
-    return (
-        <ScrollView>
-            <View style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
-                <View style={styles.container}>
 
-                    <Text style={styles.heading}>Events</Text>
+    useEffect(() => {
+        getData();
+      }, []);
+     const getData = async () => {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+
+        
+          
+        fetch("http://omshukla.pythonanywhere.com/dashboard/events/", requestOptions)
+        .then((response) => response.json())
+        .then((json) => setdata(json))
+        .catch((error) => console.error(error));
+
+        
+        }
+        console.log(data);
+
+        const renderItem = ({ item }) => {
+            return (
+                <View>
+                <ImageBackground source={require('../assets/coding.png')} resizeMode="cover" style={styles.image} >
+                <Text style={{ color: 'white', fontWeight: 'bold', position: 'absolute', bottom: hp('43%'), left: 0}}>__________________________________________</Text>
+                <Text style={{ color: 'white', fontWeight: 'bold', position: 'absolute', bottom: hp('39%'), left: hp('1%'),fontSize:25}}>{item.event_name}</Text>
+                <Text style={{ color: 'white', fontWeight: 'bold', position: 'absolute', bottom: hp('35%'), left: hp('1%'),fontSize:18}}>{item.event_commitee}</Text>
+                </ImageBackground>
                 </View>
-            </View>
-        </ScrollView>
+            );
+        };
+    return (
+                <View style={styles.container}>
+                <Text style={styles.heading}>Upcoming Events</Text>
+                <FlatList
+                data={data}
+                keyExtractor={({ id }) => id}
+                contentContainerStyle={{}}
+                renderItem={renderItem}
+                />
+                </View>
+    
+        
     );
 }
 const styles = StyleSheet.create({
@@ -38,15 +76,30 @@ const styles = StyleSheet.create({
         backgroundColor: '#151C20',
     padding: 70,
     height: hp('100%'),
+    width:wp('100%'),
+    marginLeft:hp('0%'),
+    marginRight:hp('0%'),
     },
     heading: {
-        marginTop: hp('2%'),
-        marginLeft: hp('5%'),
-        color: '#FF4E00',
-        fontSize: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
+        marginTop: hp('0%'),
+        marginLeft: hp('3%'),
+        marginRight:hp('0%'),
+        color: 'white',
+        fontSize: 25,
         fontFamily: 'Source Sans Pro',
     },
-  
+    image :{
+        height: hp('25%'),
+        width: wp('80%'),
+        margin: hp('5%'),
+        marginTop:hp('4%'),
+        marginLeft: hp('0%'),
+        marginRight:hp('0%'),
+        marginBottom:hp('2%'),
+        borderRadius: hp('2%'),
+        position: 'relative',
+
+    }
+    
+   
 });
