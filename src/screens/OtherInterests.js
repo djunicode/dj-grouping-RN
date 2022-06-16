13ec7436e202d771dd944d80422d946c41e3367d
Riya Shah <riyajshah021@gmail.com>
2022-06-16 19:16:20 +0530

    interests and other interests integrated

--- src/screens/OtherInterests.js
+++ src/screens/OtherInterests.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {
   widthPercentageToDP as wp,
@@ -6,6 +6,35 @@ import {
 } from 'react-native-responsive-screen';
 
 const OtherInterests = ({navigation}) => {
+  const Interests = ['debating','photography','finance','marketing','editorial','creative','dancing','music','painting','travelling'];
+  const [list, setList] = useState([]);
+  const [field,setfield]=useState('');
+  const addInterest=() => {
+   setList([...list,field]);
+   console.log(list)};
+
+   const Interest =()=>{
+   var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "name": list
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://omshukla.pythonanywhere.com/dashboard/interest/4/", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  
+}
+
   return (
     <View style={styles.container}>
       <View style={{flexDirection: 'row'}}>
@@ -30,46 +59,76 @@ const OtherInterests = ({navigation}) => {
         <Text style={styles.header}>Other Interests</Text>
         <View>
           <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => {setfield(Interests[0]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Photography</Text>
+              <Text style={styles.label}>{Interests[0]}</Text>
             </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {setfield(Interests[1]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Karaoke</Text>
+              <Text style={styles.label}>{Interests[1]}</Text>
             </View>
+            </TouchableOpacity>
           </View>
           <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => {setfield(Interests[2]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Photography</Text>
+              <Text style={styles.label}>{Interests[2]}</Text>
             </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {setfield(Interests[3]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Karaoke</Text>
+              <Text style={styles.label}>{Interests[3]}</Text>
             </View>
+            </TouchableOpacity>
           </View>
           <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => {setfield(Interests[4]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Photography</Text>
+              <Text style={styles.label}>{Interests[4]}</Text>
             </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {setfield(Interests[5]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Karaoke</Text>
+              <Text style={styles.label}>{Interests[5]}</Text>
             </View>
+            </TouchableOpacity>
           </View>
           <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => {setfield(Interests[6]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Photography</Text>
+              <Text style={styles.label}>{Interests[6]}</Text>
             </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {setfield(Interests[7]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Karaoke</Text>
+              <Text style={styles.label}>{Interests[7]}</Text>
             </View>
+            </TouchableOpacity>
           </View>
           <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity onPress={() => {setfield(Interests[8]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Photography</Text>
+              <Text style={styles.label}>{Interests[8]}</Text>
             </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {setfield(Interests[9]);
+                    addInterest()}}>
             <View style={styles.box}>
-              <Text style={styles.label}>Karaoke</Text>
+              <Text style={styles.label}>{Interests[9]}</Text>
             </View>
+            </TouchableOpacity>
           </View>
-          <TouchableOpacity onPress={() => navigation.navigate('Tabs')}>
+          <TouchableOpacity onPress={() =>{Interest(); navigation.navigate('Tabs')}}>
             <Image
               style={{
                 height: 50,
